e1ab62f7969106bd434bd27dccda752cdaa29b73
Marcio Silva <marcio.silva@ibm.com>
2023-06-05 16:25:42 -0300

    substitute negative test for positive one

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -14,9 +14,13 @@ describe('the palindrome canary spec', () => {
   });
 
   it('false for dad mom', () => {
-    expect((isPalindrome('dad mom'))).toBe(false);
+    expect(isPalindrome('dad mom')).toBe(false);
   });
-  it.todo ('true for whitespace');
+
+  it('true for whitespace', () => {
+    expect(isPalindrome('   ')).toBe(false)
+  });
+
   it.todo ('error for empty string');
   it.todo ('error for not a string');
 });
